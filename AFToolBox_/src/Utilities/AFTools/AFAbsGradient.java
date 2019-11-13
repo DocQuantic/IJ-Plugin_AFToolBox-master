b26e26338f2d38c801722feb5ccd18409af9ef69
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2019-11-13 12:05:30 +0000

    Added license in each file

--- AFToolBox_/src/Utilities/AFTools/AFAbsGradient.java
+++ AFToolBox_/src/Utilities/AFTools/AFAbsGradient.java
@@ -4,12 +4,28 @@ import ij.ImagePlus;
 import ij.measure.ResultsTable;
 
 /**
- * 
- */
+*
+*  AFAbsGradient v1, 23 oct. 2019 
+   William Magrini, w.magrini at yahoo.fr
+   
+   Copyright (C) 2019 William Magrini
+ 
+   License:
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 /**
  * @author William Magrini @ Bordeaux Imaging Center
- * Created 23 oct. 2019 at 16:02:42 
  * 
  */
 public class AFAbsGradient extends AFTool{
